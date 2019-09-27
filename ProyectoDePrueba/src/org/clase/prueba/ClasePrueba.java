9251ee13dd8ba7a8fd27a18c479edde3cf99f096
sergio <sergio@sergio-PC>
2019-09-27 13:22:06 -0500

    //Se creo otro proyecto llamado ProyectoPrueba

---
+++ ProyectoDePrueba/src/org/clase/prueba/ClasePrueba.java
@@ -0,0 +1,26 @@
+package org.clase.prueba;
+
+public class ClasePrueba {
+
+	private int id;
+	private String nombre;
+	
+	public static void main(String[] args) {
+		System.out.println("Un mensaje");
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
